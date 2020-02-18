2485e5556b2e1c07a6aba0bb1b58c91b3bc57f47
Michelene Chon <michelenechon@gmail.com>
2020-02-18 11:45:55 -0800

    Use the same key names as we get from the GH Jobs API (even though they are not CamelCase -- underscores work in Mongo (but dashes do not)

--- db/models/Job.js
+++ db/models/Job.js
@@ -4,14 +4,14 @@ const JobSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
   type: { type: String, required: true },
   url: { type: String, required: true },
-  createdAt: { type: String, required: false, alias: 'created_at' },
+  created_at: { type: String, required: false },
   company: { type: String, required: true },
-  companyUrl: { type: String, required: false, alias: 'company_url' },
+  company_url: { type: String, required: false },
   location: { type: String, required: true },
   title: { type: String, required: true },
   description: { type: String },
-  howToApply: { type: String, alias: 'how_to_apply' },
-  companyLogo: { type: String, alias: 'company_logo' }
+  how_to_apply: { type: String },
+  companyLogo: { type: String }
 });
 
 const Job = mongoose.model('Job', JobSchema);
