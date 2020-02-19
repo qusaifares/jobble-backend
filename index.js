de91416b0ad47037c4749abe37f6e4a8d5a94481
Michelene Chon <michelenechon@gmail.com>
2020-02-19 07:55:22 -0800

    Remove body-parser (no longer needed as of Express 4.16+)

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
 const jobsController = require('./controllers/jobs');
@@ -11,8 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/jobs', jobsController);
 app.use('/users', usersController);
