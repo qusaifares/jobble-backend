02e98d478f4c805bf572bec57c774379ae7f36cb
Michelene Chon <michelenechon@gmail.com>
2020-02-20 07:24:33 -0800

    Implement the '/:userID/save/:jobID' route

--- controllers/users.js
+++ controllers/users.js
@@ -2,17 +2,16 @@ const express = require('express');
 const User = require('../db/models/User');
 const router = express.Router();
 
+// Return only the _id field for all users
 router.get('/', (req, res) => {
   User.find({}).then(data => res.json(data.map(user => user.id)));
 });
 
-// Changed route from /login/:userId
 router.get('/:userId', (req, res) => {
   User.findOne({ _id: req.params.userId }).then(data => res.json(data));
 });
 
 router.post('/create/:username', (req, res) => {
-  console.log('THE BODY IS', req.body);
   User.create({
     username: req.params.username,
     unSavedJobs: [],
@@ -23,18 +22,16 @@ router.post('/create/:username', (req, res) => {
     .catch(console.error);
 });
 
-// When calling the /save/:jobID route, provide the username in the request body
-// Then, for that user record,
-// - Add the jobID to savedJobs
-router.put('/save/:jobID', (req, res) => {
-  const filter = { _id: req.body.id };
-  const update = { $push: { savedJobs: jobID } };
-  User.findOneAndUpdate(filter, update, { new: true });
+router.put('/:userID/save/:jobID', (req, res) => {
+  const filter = { _id: req.params.userID };
+  const update = { $push: { savedJobs: req.params.jobID } };
+  User.findOneAndUpdate(filter, update).then(user => res.json(user));
+  // Now I need to remove the jobID from unsavedJobs:
 });
 
 // - Add the jobID to discardedJobs
-router.post('/discard/:jobID', (req, res) => {
-  const filter = { username: req.body.username };
+router.post('/:userID/discard/:jobID', (req, res) => {
+  const filter = { _id: req.params.userID };
   const update = { $push: { discardedJobs: jobID } };
   User.findOneAndUpdate(filter, update, { new: true });
 });
